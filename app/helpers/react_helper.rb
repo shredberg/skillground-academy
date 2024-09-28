cd94f3217363dca614e3be28641124949c85064a
Jon Stenqvist <jon.stenqvist@equipe.com>
2024-09-28 12:38:29 -0400

    installed react

---
+++ app/helpers/react_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ReactHelper
+  def react_component(component_name, props = {})
+    tag.div(data: {
+      controller: "react",
+      react_component_value: component_name,
+      react_props_value: props
+    })
+  end
+end
